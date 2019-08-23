bc27664d77371e81496582995b8c83df7d93d7c6
Aaditya Sondhi <aaditya@autonomic.ai>
2019-08-23 10:58:46 -0400

    role flag for exec
    This lets the user specify a role ARN with the `-r` flag to assume the
    specified role.

--- cmd/exec.go
+++ cmd/exec.go
@@ -21,6 +21,7 @@ import (
 var (
 	sessionTTL    time.Duration
 	assumeRoleTTL time.Duration
+	assumeRole    string
 )
 
 func mustListProfiles() lib.Profiles {
@@ -44,6 +45,7 @@ func init() {
 	RootCmd.AddCommand(execCmd)
 	execCmd.Flags().DurationVarP(&sessionTTL, "session-ttl", "t", time.Hour, "Expiration time for okta role session")
 	execCmd.Flags().DurationVarP(&assumeRoleTTL, "assume-role-ttl", "a", time.Hour, "Expiration time for assumed role")
+	execCmd.Flags().StringVarP(&assumeRole, "assume-role", "r", "", "Assume a specified role using the role ARN")
 }
 
 func loadDurationFlagFromEnv(cmd *cobra.Command, flagName string, envVar string, val *time.Duration) error {
@@ -122,6 +124,10 @@ func execRun(cmd *cobra.Command, args []string) error {
 	profiles, err := config.Parse()
 	if err != nil {
 		return err
+	}
+
+	if assumeRole != "" {
+		profiles[profile]["role_arn"] = assumeRole
 	}
 
 	if _, ok := profiles[profile]; !ok {
